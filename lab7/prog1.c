b90fbf9d8405c23d7ae38e52d176ac823c00ec84
adithyag1 <121248641+adithyag1@users.noreply.github.com>
2023-01-21 10:36:07 +0530

    Create prog1.c

---
+++ lab7/prog1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void main(){
+    int a,b,c,d;
+    printf("Enter dimensions of matrix1 as axb ");
+    scanf("%dx%d",&a,&b);
+    printf("Enter dimensions of matrix2 as cxd ");
+    scanf("%dx%d",&c,&d);
+    int m1[a][b],m2[c][d],prod[a][d];
+    if(b!=c) printf("Invalid orders of matrix! cannot multiply");
+    else{
+        printf("Enter elements of matrix1 ");
+        for(int row=0;row<a;row++){
+            for(int col=0;col<b;col++){
+                scanf("%d",&m1[row][col]);
+            }
+        }
+        /*
+        for(int row=0;row<a;row++){
+            for(int col=0;col<b;col++){
+                printf("%d\t",m1[row][col]);
+            }
+            printf("\n");
+        }
+        */
+        
+        printf("Enter elements of matrix2 ");
+        for(int row=0;row<c;row++){
+            for(int col=0;col<d;col++){
+                scanf("%d",&m2[row][col]);
+            }
+        }
+        for(int row=0;row<a;row++){
+            for(int col=0;col<d;col++){
+                int ele=0;
+                for(int i=0;i<b;i++){
+                    ele+=m1[row][i]*m2[i][row];
+                    
+                }
+                //prod[row][col]=ele;
+                printf("%d\t",
+            }
+        }
+        
+    }
+}
