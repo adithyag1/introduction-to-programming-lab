0a636d1ea8cbdfb4e85bb24bd3764e04fada9a91
Adithya G <121248641+adithyag1@users.noreply.github.com>
2023-01-25 14:07:45 +0530

    Update prettyprint.c

--- lab7/prettyprint.c
+++ lab7/prettyprint.c
@@ -1,6 +1,5 @@
 for(int row=0;row<n;row++){
         for(int col=0;col<n;col++){
-            arr[row][col]=arr[row][n-1-col];
             printf("%f\t",arr[row][col]);
             }
         printf("\n");
