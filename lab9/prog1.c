1d255a80e65213084931db14d40511fcd02f3ffc
Adithya G <121248641+adithyag1@users.noreply.github.com>
2023-02-11 13:56:21 +0530

    Update prog1.c

--- lab9/prog1.c
+++ lab9/prog1.c
@@ -1,12 +1,13 @@
 # include <stdio.h>
-
+void swap(int *ptr1,int *ptr2){
+    int temp=*ptr1;
+    *ptr1=*ptr2;
+    *ptr2=temp;
+}
 void main(){
     int x,y;
     printf("Enter x and y: ");
     scanf("%d %d",&x,&y);
-    int *ptrx=&x,*ptry=&y;
-    int temp=*ptrx;
-    *ptrx=*ptry;
-    *ptry=temp;
+    swap(&x,&y);
     printf("x=%d, y=%d",x,y);
 }
